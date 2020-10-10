ce4f6507f79e19753d279621134a1ce103e99c81
kwesi Welbred <infofeedacc@gmail.com>
2020-10-10 05:51:37 -1200

    added another implementation of list methods

--- list.py
+++ list.py
@@ -54,5 +54,57 @@ eg2.
 myList[0 :-1]
 myList[-3:-1]
 myList[:-1]
- 
 
+
+"Loop through a List using for loop"
+thislist = ["apple", "banana", "cherry"]
+for x in thislist:
+    print(x)
+
+"""Check if Item Exists: "To determine if a specified item is present in a list use the in keyword:
+Check if "apple" is present in the list:
+"""
+thislist = ["apple", "banana", "cherry"]
+if "apple" in thislist:
+  print("Yes, 'apple' is in the fruits list")
+
+
+"""
+There are several ways to join, or concatenate, two or more lists in Python
+One of the easiest ways are by using the + operator.
+"""
+list1 = ["a", "b" , "c"]
+list2 = [1, 2, 3]
+
+list3 = list1 + list2
+print(list3)
+
+"""
+Another way to join two lists are by appending all the items from list2 into list1, one by one:
+Append list2 into list1 Or you can use the extend() method, which purpose is to add elements from one list to another list:
+"""
+list1 = ["a", "b" , "c"]
+list2 = [1, 2, 3]
+
+for x in list2:
+  list1.append(x)
+
+print(list1)
+
+"Use the extend() method to add list2 at the end of list1:"
+list1 = ["a", "b" , "c"]
+list2 = [1, 2, 3]
+
+list1.extend(list2)
+print(list1)
+
+
+"""
+The list() Constructor
+It is also possible to use the list() constructor to make a new list.
+Example
+Using the list() constructor to make a List: 
+
+"""
+thislist = list(("apple", "banana", "cherry")) # note the double round-brackets
+print(thislist)
